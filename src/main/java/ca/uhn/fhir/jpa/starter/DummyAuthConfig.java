d16e19150b803cf70e03419f3f41b1afccbe15a7
Tadgh <garygrantgraham@gmail.com>
2020-07-07 15:42:48 -0700

    Add cors to login endpoint

--- src/main/java/ca/uhn/fhir/jpa/starter/DummyAuthConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/DummyAuthConfig.java
@@ -2,11 +2,15 @@ package ca.uhn.fhir.jpa.starter;
 
 import ca.uhn.fhir.jpa.starter.controller.DummyAuthController;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.HttpHead;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -17,12 +21,45 @@ import java.util.List;
 @EnableWebMvc
 public class DummyAuthConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+      converters.add(new MappingJackson2HttpMessageConverter());
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    if (HapiProperties.getCorsEnabled()) {
+      registry.addMapping("/login")
+        .allowedOrigins(
+          HapiProperties.getCorsAllowedOrigin().split(",")
+        )
+        .allowedHeaders(
+          HttpHeaders.ORIGIN,
+          HttpHeaders.ACCEPT,
+          HttpHeaders.CONTENT_TYPE,
+          HttpHeaders.AUTHORIZATION,
+          HttpHeaders.CACHE_CONTROL,
+          "x-fhir-starter",
+          "X-Requested-With",
+          "Prefer"
+        ).exposedHeaders(
+          HttpHeaders.LOCATION,
+          HttpHeaders.CONTENT_LOCATION
+        ).allowedMethods(
+         "GET",
+         "POST",
+         "PUT",
+         "DELETE",
+         "OPTIONS",
+         "PATCH",
+         "HEAD"
+      );
     }
 
-    @Bean
+
+  }
+
+  @Bean
     public ObjectMapper objectMapper() {
         Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
         b.indentOutput(true);
